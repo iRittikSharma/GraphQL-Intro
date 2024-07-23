af11703616f07e29a9e5023ab746b9819642b685
AgentRittik <109760373+AgentRittik@users.noreply.github.com>
2024-07-23 21:03:45 +0530

    completed Basic GraphQl Server

--- src/server.js
+++ src/server.js
@@ -3,11 +3,35 @@ import { ApolloServer, gql } from "apollo-server";
 // create a new Apollo server object using the ApolloServer constructor
 // gql - > helps us to define graphql query in the form of a string
 
+const Todos = [
+  {
+    id: 1,
+    task: "Getting started with GRAPHQL",
+    completed: true,
+  },
+  {
+    id: 2,
+    task: "Understanding Graphql on server side",
+    completed: true,
+  },
+  {
+    id: 3,
+    task: "Understanding Graphql On client side",
+    completed: false,
+  },
+];
+
 // Defining the types in graphql
+// ! -> defines that these properties are mandatory
 const typeDefs = gql`
   type Todo {
+    id: ID!
     task: String
     completed: Boolean
+  }
+  type Query {
+    getAllTodos: [Todo]
+    getTodo(id: ID!): Todo
   }
 `;
 
@@ -15,15 +39,29 @@ const typeDefs = gql`
  *  Resolvers : are the those fns which have the logic and gets executed when client queries for something
  */
 
+const resolvers = {
+  Query: {
+    getAllTodos: () => {
+      //How the client get the data is defined here ->  it can be a db call or api call or anyhting
+      return Todos;
+    },
+    getTodo: (_, param) => {
+      console.log(param);
+      return Todos.find((todo) => todo.id == param.id);
+    },
+  },
+};
+
 const server = new ApolloServer({
-  typeDefs: undefined,
-  resolvers: undefined,
+  typeDefs: typeDefs,
+  resolvers: resolvers,
 });
 
 server.listen().then(() => {
   console.log("GraphQl server started");
 });
 
+// by default this serveer will bbe started at port 4000
 /***
  *      Topics can refer later on
  *
